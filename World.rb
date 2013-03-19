f7334f4e01c39a3dfd24d5b17a2139480bb2c868
Kevin Grant <userkevin41@gmail.com>
2013-03-18 21:06:51 -0700

    Game Test moved

--- World.rb
+++ World.rb
@@ -1,34 +1,48 @@
 require 'rexml/document'
 include REXML
 require './Room.rb'
+require './User.rb'
 class World
+  
+attr_accessor :user
 
  # Describes world behavior
-  def initialize(xmlFilePath)
-    xmlfile = File.new(xmlFilePath)
-    xmldoc = Document.new(xmlfile)
-    root = xmldoc.root
+  def initialize(xmlFilePath,user)
+    @user = user
     @rooms = Hash.new
+    
+    # Process the level which is saved as XML, the loop will create Room objects populate the @rooms hash for later use 
+    xmldoc = Document.new(File.new(xmlFilePath))
+    root = xmldoc.root
     xmldoc.elements.each("Level/Room/Title"){ 
         |e| room = Room.new(e.text,e.attributes["North"],e.attributes["East"],e.attributes["South"],e.attributes["West"])
         @rooms[room.title] = room
      }
+     
+     assignUserRandomRoom
 
   end
   
+  #debug print method
   def printRooms
     @rooms.each {|key, value| puts "#{key} is #{value}" }
   end
   
-  def 
+  # This method is for setting the user into a random room, upon initialization and upon getting attacked.
+  def assignUserRandomRoom
+    @hashkeys = @rooms.keys
+    user.currentRoom = @rooms[@hashkeys[rand(@hashkeys.length)]]
+  end
+ 
  
  
  
  
 end
 
-world = World.new("sampleLevel.xml");
+world = World.new("sampleLevel.xml",User.new("Kevin"));
 world.printRooms
+puts world.user.currentRoom
 
 
 
