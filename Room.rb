2154c60cddaed958d86dae041afb45d478f597b6
Kevin Grant <userkevin41@gmail.com>
2013-03-18 20:25:00 -0700

    world and room updated

--- Room.rb
+++ Room.rb
@@ -1,3 +1,27 @@
-class World
+class Room
+  attr_accessor :title
+  
+  def initialize(title, north, east, south, west)
+    @title=title
+    @paths = [north,east,south,west]
+  end
+  
+  def north
+    @paths[0]
+  end
+  
+    def east
+    @paths[1]
+  end
+  
+   def south
+    @paths[2]
+  end
+  
+   def west
+    @paths[3]
+  end
+  
+
  # Describes world behavior
 end
--- World.rb
+++ World.rb
@@ -1,3 +1,34 @@
+require 'rexml/document'
+include REXML
+require './Room.rb'
 class World
+
  # Describes world behavior
+  def initialize(xmlFilePath)
+    xmlfile = File.new(xmlFilePath)
+    xmldoc = Document.new(xmlfile)
+    root = xmldoc.root
+    @rooms = Hash.new
+    xmldoc.elements.each("Level/Room/Title"){ 
+        |e| room = Room.new(e.text,e.attributes["North"],e.attributes["East"],e.attributes["South"],e.attributes["West"])
+        @rooms[room.title] = room
+     }
+
+  end
+  
+  def printRooms
+    @rooms.each {|key, value| puts "#{key} is #{value}" }
+  end
+  
+ 
+ 
+ 
+ 
+ 
 end
+
+world = World.new("sampleLevel.xml");
+world.printRooms
+
+
+
